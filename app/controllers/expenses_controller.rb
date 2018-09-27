22c1668dfb3e5835b938a970b5327dee927efb54
enciie <enciie@gmail.com>
2018-09-27 15:25:49 -0700

    Create expense form layout

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,2 +1,13 @@
 class ExpensesController < ApplicationController
+  before_action :authenticate_user, only: [:index, :create, :edit, :upate, :destroy]
+
+  def new
+    @group = Group.find_by(id: params[:group_id])
+    @expense = Expense.new
+  end
+
+  def create
+    raise params.inspect
+  end
+
 end
