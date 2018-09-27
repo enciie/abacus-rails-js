a2789903fa133ce381cd05255de7cce3254a3b36
enciie <enciie@gmail.com>
2018-09-26 22:47:24 -0700

    Create sessions#create, users#show, #current_user, #logged_in?, and #authenticate_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,27 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
 
   def home
   end
-  
+
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+  # methods you build in controller do not permeate to your ActionView
+  # you have to explicitly tell it by calling it a helper method
+
+  def logged_in?
+    !!current_user
+  end
+
+  def authenticate_user
+    if !logged_in?
+      redirect_to root_path
+    end
+  end
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
   private
 
   def user_params
