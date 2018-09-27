2376cc790f849b7849eed4ae8a313729ffd4bde1
enciie <enciie@gmail.com>
2018-09-26 22:16:48 -0700

    Create sign up form and add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  # Validations
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true,
+                    format: { with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9\.-]+\.[A-Za-z]+\Z/ }
+  validates :password, presence: true
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'application#home'
-  get '/signup', to: 'users#new'
+  get '/signup' => 'users#new'
+
+  resources :users, only: [:create, :show, :edit, :update]
 
 end
