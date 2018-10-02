96ba5c7c4d3c5e2a0350efc39d0007c05dfaca78
enciie <enciie@gmail.com>
2018-10-01 20:10:06 -0700

    Add flash message for sucessfully signing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:notice] = "Successfully Created An Account"
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
