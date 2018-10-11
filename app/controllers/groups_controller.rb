26ed03ef98b7cf1b4d64f3d737f3a8a4351ea118
enciie <enciie@gmail.com>
2018-10-11 06:27:09 -0700

    Remove the sort link method

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,7 +3,7 @@ class GroupsController < ApplicationController
   before_action :set_group, only: [:show, :edit, :update, :destroy]
 
   def index
-    @groups = Group.order("#{sort_column} #{sort_direction}")
+    @groups = Group.all
   end
 
   def new
