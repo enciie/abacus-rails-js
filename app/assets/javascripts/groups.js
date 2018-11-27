7e375f715b1ab0bea5ddc30ee88860be6f558c75
enciie <enciie@gmail.com>
2018-11-27 00:20:07 -0800

    Create object prototype method to create the html for new group

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 //end of document ready
 
 function createGroup(){
-  $("form.new_group").on("submit", (event)=> {
+  $("form.new_group").on("submit", function(event){
     event.preventDefault();
     debugger
     $.ajax({
@@ -20,18 +20,20 @@ function createGroup(){
           $("#group_name").val("")
           //creates new instance of our group model
           let group = new Group(response)
+          debugger
           // add newly created group to the top of the table
-          let trHTML = "";
-          trHTML += '<tr><td>' + group.name + '</td><td>' + group.status + '</td>'
-          trHTML += '<td>' + `<a class="glyphicon glyphicon-eye-open" id="eye-icon" href="/groups/${group.id}"></a>` + '</td>'
-          trHTML += '<td>' + `<a class="glyphicon glyphicon-pencil" id="pencil-icon" href="/groups/${group.id}/edit">` +  '</td>'
-          trHTML += '<td>' + `<a data-confirm="Are you sure?" class="glyphicon glyphicon-trash" id="trash-icon" rel="nofollow" data-method="delete" href="/groups/${group.id}"></a>` + '</td></tr>'
-            if (group.status === "Active") {
-              $("div.active_groups table").prepend(trHTML)
-            } else {
-              $("div.inactive_groups table").prepend(trHTML)
-            }
-            //end of if/else
+          group.addGroupHtml()
+          // let trHTML = "";
+          // trHTML += '<tr><td>' + group.name + '</td><td>' + group.status + '</td>'
+          // trHTML += '<td>' + `<a class="glyphicon glyphicon-eye-open" id="eye-icon" href="/groups/${group.id}"></a>` + '</td>'
+          // trHTML += '<td>' + `<a class="glyphicon glyphicon-pencil" id="pencil-icon" href="/groups/${group.id}/edit">` +  '</td>'
+          // trHTML += '<td>' + `<a data-confirm="Are you sure?" class="glyphicon glyphicon-trash" id="trash-icon" rel="nofollow" data-method="delete" href="/groups/${group.id}"></a>` + '</td></tr>'
+          //   if (group.status === "Active") {
+          //     $("div.active_groups table").prepend(trHTML)
+          //   } else {
+          //     $("div.inactive_groups table").prepend(trHTML)
+          //   }
+          //   //end of if/else
         }
         //end of if/else
       }
@@ -74,10 +76,42 @@ class Group {
     if (json.status === 0){
       this.status = "Active"
     } else {
-      this.status == "Inactive"
+      this.status = "Inactive"
     }
     //end of if/else
   }
   //end of constructor
+
+  //Sets a method on object prototype
+  addGroupHtml(){
+      // adds the newly created group to top of the table
+      let trHTML = "";
+          trHTML += '<tr><td>' + this.name + '</td><td>' + this.status + '</td>'
+          trHTML += '<td>' + `<a class="glyphicon glyphicon-eye-open" id="eye-icon" href="/groups/${this.id}"></a>` + '</td>'
+          trHTML += '<td>' + `<a class="glyphicon glyphicon-pencil" id="pencil-icon" href="/groups/${this.id}/edit">` +  '</td>'
+          trHTML += '<td>' + `<a data-confirm="Are you sure?" class="glyphicon glyphicon-trash" id="trash-icon" rel="nofollow" data-method="delete" href="/groups/${this.id}"></a>` + '</td></tr>'
+      if (this.status === "Active") {
+        $("div.active_groups table").prepend(trHTML)
+      } else {
+        $("div.inactive_groups table").prepend(trHTML)
+      }
+      //end of if else
+    }
+    //end of prototype addGroupHtml
 }
 //end of class Group
+
+// Group.prototype.addGroupHtml = function(){
+//     // adds the newly created group to top of the table
+//     let trHTML = "";
+//         trHTML += '<tr><td>' + this.name + '</td><td>' + this.status + '</td>'
+//         trHTML += '<td>' + `<a class="glyphicon glyphicon-eye-open" id="eye-icon" href="/groups/${this.id}"></a>` + '</td>'
+//         trHTML += '<td>' + `<a class="glyphicon glyphicon-pencil" id="pencil-icon" href="/groups/${this.id}/edit">` +  '</td>'
+//         trHTML += '<td>' + `<a data-confirm="Are you sure?" class="glyphicon glyphicon-trash" id="trash-icon" rel="nofollow" data-method="delete" href="/groups/${this.id}"></a>` + '</td></tr>'
+//     if (this.status === "Active") {
+//       $("div.active_groups table").prepend(trHTML)
+//     } else {
+//       $("div.inactive_groups table").prepend(trHTML)
+//     }
+//     //end of if else
+//   }
