c1d131983852b0495202e523e71c6caca8c0f89e
enciie <enciie@gmail.com>
2018-11-27 03:59:54 -0800

    Add groupId to update summary button and tables

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -65,7 +65,11 @@ function attachGroupListeners(){
   // });
 
   $("div#group-expenses-page").on("click", "#group-summary-reload", (e)=> {
-    $("div.group-summary-tables").load(window.location.href + " div.group-summary-tables" );
+    debugger
+    let groupId = parseInt($("#group-summary-reload").attr("data-groupid"))
+    let url = "/groups/" + groupId
+    debugger
+    $("div.group-summary-tables").load(url + " div.group-summary-tables" );
   });
   //end of group-summary reload
 
